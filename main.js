1fe72aac6987cbaadc0d72abe2f3762c85a71ea2
Crystal K Young <maybiz@hotmail.com>
2020-11-02 13:23:56 -0500

    add

--- main.js
+++ main.js
@@ -1,11 +1,67 @@
 // Your code below.
 
+const isValidPassword = function(str) {
+  for (const check of str) {
+    if (str.includes(' ') || str.includes('.') || str.length < 12) {
+      return false
+    } else {
+      return true
+    }
+  }
+}
 
+const onlyCs = function (nums) { 
+  let Cs = [] 
+  for (let i = 0; i < nums.length; i++) {
+  if (nums[i] >= 70 && nums[i] <= 79) {
+    Cs.push(nums[i])
+    }
+  }
+  return Cs
+}
 
+const countBs = function (letterB) {
+  let bees = 0
+  for (let i = 0; i < letterB.length; i++) {
+    if (letterB[i] >= 80 && letterB[i] <= 89) {
+      bees = bees + 1
+    }
+  } 
+  return bees
+}
 
+const lastIndexOfPunctuation = function (str) {
+  let exclaim = str.lastIndexOf('!')
+  let period = str.lastIndexOf('.')
+  let question = str.lastIndexOf('?')
+  for (const marks of str) {
+      if (str.includes('!')) {
+        return exclaim
+      } else if (str.includes('?')) {
+        return question
+      } else if (str.includes('.')) {
+        return period
+    } else {
+      return -1
+    }
+  }
+}
 
+const deleteMiddleLetters = function(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+  if (str.length % 2 === 1) {
+    result = result + str[i] / 2
+  } else if (str[i].length / 2  && str[i].length / 2 -1) {
+      result = result + str[i]
+  }
+  }
+return result
+}
 
+const getCenturies = function () {
 
+}
 
 
 
